564e37474909f45e90ffb8101f152855e70e7a2a
iDubrovsky <iapopko1@mts.ru>
2021-05-02 12:08:19 +0300

    Выполнил 3-е задание из дз к 1-ой лекции

--- 1I_Dubrovsky.playground/Contents.swift
+++ 1I_Dubrovsky.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-// 1. Решить квадратное уравнение.
+// MARK: 1. Решить квадратное уравнение.
 // Создал функцию 'quadratic' с помощью которой можно рещшить квадратное уравнение.
 
 func quadratic(a: Int, b: Int, c: Int) -> [Int] {
@@ -28,30 +28,46 @@ func quadratic(a: Int, b: Int, c: Int) -> [Int] {
     }
 }
 
-//var primer = quadratic(a: 1, b: 12, c: 36)
+//var result_1 = quadratic(a: 1, b: 12, c: 36)
+//print(result_1)
 
 
 
-//2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
+// MARK: 2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
 
-func triangle(a: Double, b: Double) -> [Character: Int] {
+func triangle(a: Double, b: Double) -> [Character: Double] {
     // Создал функцию, которая добавляет необходимые данные в словарь.
     // Элементы можно найти по соответстующим им названиям (S - площадь, P - периметр, H - гипотенуза).
     if a > 0 && b > 0 {
         let s: Double = 1 / 2 * a * b
         let h: Double = sqrt((a * a) + (b * b))
         let p: Double = a + b + h
-        print("Площадь равна \(Int(s)).\nПериметр равен \(Int(p)).\nГипотенуза равна \(Int(h)).")
-        return["S": Int(s), "P": Int(p), "H": Int(h)]
+        print("Площадь равна \(s).\nПериметр равен \(p).\nГипотенуза равна \(h).")
+        return["S": s, "P": p, "H": h]
     } else {
         print("Значение катетов не может быть меньше 0. Возвращаю пустое множество.")
         return [:]
     }
 }
 
-var f = triangle(a: 5, b: 2)
-print(f["P"])
+//var result_2 = triangle(a: 5, b: 2)
+//print(result_2)
+//print(result_2["P"])
 
 
 
-//3. * Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
+// MARK: 3. * Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет.
+
+func searchForContribution (contribution c: Double, percent p: Int, year y: Int = 5) -> Double? {
+    if p > 0 && p < 100 && c > 0 {
+        let o = ((c / 100 * Double(p)) * Double(y)) + c
+        print("Сумма вклада через \(y) год/лет будет равна \(o).")
+        return o
+    } else {
+        print("Не выполнено одно из условий (сумма вклада должна быть больше 0, процент должен быть больше 0 и меньше 100.)")
+        return nil
+    }
+}
+
+//var result_3 = searchForContribution(contribution: 1000, percent: 10)
+//print(result_3)
